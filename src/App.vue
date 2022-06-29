d20f5d535deeb53c5bd28a260398589b1c97e3d0
daviddpi <piscopodavid97@gmail.com>
2022-06-29 17:43:46 +0200

    v-bind + conditional rendering

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,196 @@
 <template>
-
   <div>    
-    <h1>Ciao Mondo!</h1>
-  </div>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <img src="./assets/logo.png">
+        </a>
+      </div>
 
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item">
+            {{home}}
+          </a>
+
+          <a class="navbar-item">
+            Documentation
+          </a>
+
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link">
+              More
+            </a>
+
+            <div class="navbar-dropdown">
+              <a class="navbar-item">
+                About
+              </a>
+              <a class="navbar-item">
+                Jobs
+              </a>
+              <a class="navbar-item">
+                Contact
+              </a>
+              <hr class="navbar-divider">
+              <a class="navbar-item">
+                Report an issue
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <a class="button is-primary">
+                <strong>Sign up</strong>
+              </a>
+              <a class="button is-light">
+                Log in
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+
+    <div class="section">
+      <div class="columns">
+        <div class="column">
+
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-square">
+                <img src="./assets/watch.png" alt="Placeholder image">
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="media">
+                <div class="media-left">
+                  <figure class="image is-48x48">
+                    <img src="./assets/watch.png" alt="Placeholder image">
+                  </figure>
+                </div>
+                <div class="media-content">
+                  <p class="title is-4">Watch</p>
+                  <p v-if="watch.inStock" class="subtitle is-6 tag is-success">130€</p>
+                  <p v-else class="subtitle is-6 tag is-warning">Non disponibile</p>
+                </div>
+              </div>
+
+              <div class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                <a href="#">#css</a> <a href="#">#responsive</a>
+                <br>
+                <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+              </div>
+            </div>
+          </div> 
+
+        </div>
+
+        <div class="column">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-square">
+                <img src="./assets/shoe.png" alt="Placeholder image">
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="media">
+                <div class="media-left">
+                  <figure class="image is-48x48">
+                    <img src="./assets/shoe.png" alt="Placeholder image">
+                  </figure>
+                </div>
+                <div class="media-content">
+                  <p class="title is-4">Stan Smith</p>
+                  <p v-if="shoes.inStock" class="subtitle is-6 tag is-success">90€</p>
+                  <p v-else class="subtitle is-6 tag is-warning">Non disponibile</p>
+                </div>
+              </div>
+
+              <div class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                <a href="#">#css</a> <a href="#">#responsive</a>
+                <br>
+                <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+              </div>
+            </div>
+          </div>       
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-square">
+                <img src="./assets/glasses.png" alt="Placeholder image">
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="media">
+                <div class="media-left">
+                  <figure class="image is-48x48">
+                    <img src="./assets/glasses.png" alt="Placeholder image">
+                  </figure>
+                </div>
+                <div class="media-content">
+                  <p class="title is-4">Gucci</p>
+                  <p v-if="glasses.inStock" class="subtitle is-6 tag is-success">120€</p>
+                  <p v-else class="subtitle is-6 tag is-warning">Non disponibile</p>
+
+                </div>
+              </div>
+
+              <div class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                <a href="#">#css</a> <a href="#">#responsive</a>
+                <br>
+                <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-square">
+                <img :src="srcBag" alt="Placeholder image">
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="media">
+                <div class="media-left">
+                  <figure class="image is-48x48">
+                    <img :src="srcBag" alt="Placeholder image">
+                  </figure>
+                </div>
+                <div class="media-content">
+                  <p class="title is-4">Armani</p>
+                  <p v-if="bag.inStock" class="subtitle is-6 tag is-success">90€</p>
+                  <p v-else class="subtitle is-6 tag is-warning">Non disponibile</p>
+                </div>
+              </div>
+
+              <div class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                <a href="#">#css</a> <a href="#">#responsive</a>
+                <br>
+                <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
 </template>
 
 <script>
@@ -12,6 +199,24 @@ export default {
   name: 'App',
   components: {
     
+  },
+  data(){
+    return {
+      home: "Home",
+      srcBag: require("./assets/bag.png"),
+      bag: {
+        inStock: true
+      },
+      shoes: {
+        inStock: false
+      },
+      glasses: {
+        inStock: true
+      },
+      watch: {
+        inStock: false
+      }
+    }
   }
 }
 </script>
